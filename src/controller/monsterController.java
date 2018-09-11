71800b52587dbe2608b5038b3b84094cc76172e9
SpencerBillings <spencerbillings2000@gmail.com>
2018-09-11 07:40:49 -0600

    Created the project and added a README file

---
+++ src/controller/monsterController.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class monsterController
+{
+
+	public monsterController()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/controller/monsterRunner.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class monsterRunner
+{
+
+	public monsterRunner()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
