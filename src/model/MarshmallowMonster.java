87599c99679c9f16984ec55780450d8e62ae060f
SpencerBillings <spencerbillings2000@gmail.com>
2018-09-12 08:07:51 -0600

    Got the getters and set the setters

--- src/model/MarshmallowMonster.java
+++ src/model/MarshmallowMonster.java
@@ -23,6 +23,7 @@ public class MarshmallowMonster
 	
 	public MarshmallowMonster(String name, double legCount, int eyeCount, boolean hasNoses, int armCount)
 	{
+		this.name = name;
 		this.legCount = legCount;
 		this.eyeCount = eyeCount;
 		this.hasNoses = hasNoses;
@@ -30,10 +31,62 @@ public class MarshmallowMonster
 		//'this.' belongs to the class
 		//parameters belong to the curly braces
 	}
+
+	// Getters
+	public String getName()
+	{
+		return name;
+	}
+	
+	public double getLegCount()
+	{
+		return legCount;
+	}
+	
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}
+	
+	public boolean getHasNoses()
+	{
+		return hasNoses;
+	}
+	
+	public int getArmCount()
+	{
+		return armCount;
+	}
+	
+	// Setters - All setters have a return type of void.
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setLegCount(double legs)
+	{
+		this.legCount = legs;
+	}
+	
+	public void setEyeCount(int eyes)
+	{
+		this.eyeCount = eyes;
+	}
+	
+	public void setHasNoses(boolean hasNoses)
+	{
+		this.hasNoses = hasNoses;
+	}
+	
+	public void setArmCount(int arms)
+	{
+		this.armCount = arms;
+	}
 	
 	public String toString()
 	{
-		String description = "My monster's name is " + name + "." + "It has " + legCount + " legs, and the scary thing it says is: Боже мой!";
+		String description = "My monster's name is " + name + ". It has " + legCount + " legs, and the scary thing it says is: Боже мой!";
 		
 		return description;
 	}
